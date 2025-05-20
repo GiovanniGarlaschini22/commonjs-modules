18a02dd6a02ad6aeb3d96c48972e586fb5ed6052
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-05-20 16:18:38 +0200

    Feat: funzione creata con 3 valori di ritorno (stringhe) - funzione esportata

---
+++ hobbies.js
@@ -0,0 +1,13 @@
+/* STEP 2: hobbies.js
+1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree.
+    La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+2. Esporta la funzione dal file.
+*/
+
+// creazione funzione con valori di ritorno //
+function hobby(hobbyOne, hobbyTwo, hobbyThree) {
+  return ["Pesca", "Lettura", "Cinema"];
+};
+
+// funzione esportata //
+export { hobby };
